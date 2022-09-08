c768e461c5c8c69fb2d461fe6848863a87241712
Colin Cheung <ColinCee@users.noreply.github.com>
2022-09-08 22:12:50 +0100

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import type { NextPage } from "next";
 import Head from "next/head";
-import { trpc } from "../utils/trpc";
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
   return (
     <>
       <Head>
@@ -13,45 +10,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Create <span className="text-purple-300">T3</span> App
-        </h1>
-        <p className="text-2xl text-gray-700">This stack uses:</p>
-        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
-          <TechnologyCard
-            name="NextJS"
-            description="The React framework for production"
-            documentation="https://nextjs.org/"
-          />
-          <TechnologyCard
-            name="TypeScript"
-            description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-            documentation="https://www.typescriptlang.org/"
-          />
-          <TechnologyCard
-            name="TailwindCSS"
-            description="Rapidly build modern websites without ever leaving your HTML"
-            documentation="https://tailwindcss.com/"
-          />
-          <TechnologyCard
-            name="tRPC"
-            description="End-to-end typesafe APIs made easy"
-            documentation="https://trpc.io/"
-          />
-          <TechnologyCard
-            name="Next-Auth"
-            description="Authentication for Next.js"
-            documentation="https://next-auth.js.org/"
-          />
-          <TechnologyCard
-            name="Prisma"
-            description="Build data-driven JavaScript & TypeScript apps in less time"
-            documentation="https://www.prisma.io/docs/"
-          />
-        </div>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+      <main className="grid place-items-center h-screen ">
+        <div className="flex flex-col">
+          <h1 className="overflow-hidden border-r-2 border-orange-500 whitespace-nowrap tracking-widest animate-typing text-5xl my-0 mx-auto inline-block">
+            Hello! My name is Colin
+          </h1>
         </div>
       </main>
     </>
@@ -59,30 +22,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  );
-};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        typing: {
+          from: { width: 0 },
+          to: { width: "100%" },
+        },
+        "blink-caret": {
+          from: { "border-color": "transparent" },
+          to: { "border-color": "transparent" },
+          "50%": { "border-color": "orange" },
+        },
+      },
+      animation: {
+        typing: "typing 2s steps(30, end), blink-caret .75s step-end infinite",
+      },
+    },
   },
   plugins: [],
 };
